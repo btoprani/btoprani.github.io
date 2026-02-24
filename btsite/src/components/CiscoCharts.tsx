fbaf61182661a45c546e8873ef510f50248a56a3
unknown <bhaven_toprani@yahoo.com>
2026-02-24 14:32:40 -0800

    updated text for clarity

--- btsite/src/components/CiscoCharts.tsx
+++ btsite/src/components/CiscoCharts.tsx
@@ -12,38 +12,26 @@ const Pietle = styled.span`
     text-align: center;
 `;
 
-const fullDataset = [{
-    label: 'Percent Product Managers',
-    data: [100],
-    backgroundColor: ['#373C42'],
-    hoverOffet: 4
-}];
-
 const eightyDataset = [{
     label: 'Percent Stakeholders',
     data: [80, 20],
-    backgroundColor: ['#373C42', '#1D69CC'],
+    backgroundColor: ['#1D69CC', '#373C42'],
     hoverOffset: 4
 }];
 
 const sixtySixDataset = [{
     label: 'Percent Stakeholders',
     data: [66, 33],
-    backgroundColor: ['#373C42', '#1D69CC'],
+    backgroundColor: ['#1D69CC', '#373C42'],
     hoverOffset: 4
 }];
 
 const utData1 = {
-    labels: ["Asked to reorder Home Page widgets"],
-    datasets: fullDataset
-};
-
-const utData2 = {
     labels: ["Asked for better customizability", "Did not mention customizability"],
     datasets: eightyDataset
 };
 
-const utData3 = {
+const utData2 = {
     labels: ["Wanted to see more AI-based insights and recommendations for changes since user's last login/shift", "Were satisfied with the AI customization at this juncture"],
     datasets: sixtySixDataset
 };
@@ -51,16 +39,12 @@ const utData3 = {
 export const UsabilityTestingCharts = () => {
     return <PieBox>
         <PieChart>
-            <Pietle>Product Managers requesting Home Page Widget reorder to emphasize changes since last login/shift</Pietle>
+            <Pietle>Stakeholders asking for better customizability</Pietle>
             <PieContainer><Pie data={utData1} /></PieContainer>
         </PieChart>
         <PieChart>
-            <Pietle>Stakeholders asking for better customizability</Pietle>
-            <PieContainer><Pie data={utData2} /></PieContainer>
-        </PieChart>
-        <PieChart>
             <Pietle>Stakeholders wanting more AI-based insights and recommendations for changes since last login/shift</Pietle>
-            <PieContainer><Pie data={utData3} /></PieContainer>
+            <PieContainer><Pie data={utData2} /></PieContainer>
         </PieChart>
     </PieBox>
 }
